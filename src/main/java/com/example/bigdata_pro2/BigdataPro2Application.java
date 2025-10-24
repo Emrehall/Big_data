4851764e5152b9abb69a8495140e1417fc57ad1c
Emrehall <ehalli460@gmail.com>
2025-10-25 02:24:42 +0300

    Spring Boot proje yapısını ve .gitignore dosyasını ekle

---
+++ src/main/java/com/example/bigdata_pro2/BigdataPro2Application.java
@@ -0,0 +1,13 @@
+package com.example.bigdata_pro2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataPro2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigdataPro2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bigdata_pro2/BigdataPro2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigdata_pro2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataPro2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
